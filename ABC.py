418fbf9d8591f61c2449e894903e665a23c162e1
shashank01ss <92664026+shashank01ss@users.noreply.github.com>
2022-10-01 14:54:32 +0530

    Update ABC.py

--- ABC.py
+++ ABC.py
@@ -24,3 +24,19 @@ if flag:
 else:
     print(num, "is a prime number")
 return 0;
+
+import math
+pi = math.pi
+def circle(radius):
+     return pi * radius**2
+ 
+def cube(side):
+     return side**3
+ 
+def cylinder(radius, height):
+     return 2*pi*radius + 2*pi*height
+ 
+def sphere(radius):
+     return 2*pi*(radius**2)
+ 
+print(circle(10))
