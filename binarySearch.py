34e38cf80e1ddc1b5e63c0cba2cdb7c5aa851a78
DivyanshuKandpal <67413629+DivyanshuKandpal@users.noreply.github.com>
2022-10-01 13:20:30 +0530

    Update binarySearch.py

--- binarySearch.py
+++ binarySearch.py
@@ -37,3 +37,12 @@ if result != -1:
     print("Element is present at index", str(result))  
 else:  
     print("Element is not present in list1") 
+
+    if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial
