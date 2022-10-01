85f1b6aa9dd09d786630a4fab3c5f503a9d9fb76
shashank01ss <92664026+shashank01ss@users.noreply.github.com>
2022-10-01 14:42:39 +0530

    Update program.py

--- program.py
+++ program.py
@@ -9,3 +9,30 @@ for num in range(lower,upper + 1):
                break  
        else:  
            print(num)  
+          
+          
+def add(a,b):
+     return a+b
+ 
+def sub(a,b):
+     return a-b
+ 
+def prod(a,b):
+     return a * b
+ 
+def div(a,b):
+     return a / b
+ 
+def si(p, r, t):
+    return (p*r*t) / 100
+ 
+def ci(p, r ,t):
+    return p * pow((1 + r/100), t)
+ 
+def sqr(num):
+    return num**2
+ 
+def sqrt(num)
+    return num**0.5
+ 
+print(add(10,15))
