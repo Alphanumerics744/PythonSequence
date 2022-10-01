03cb4b815d5c9b7ad344d4472cbc134025782e79
DivyanshuKandpal <67413629+DivyanshuKandpal@users.noreply.github.com>
2022-10-01 14:10:12 +0530

    Update divyanshu.py

--- divyanshu.py
+++ divyanshu.py
@@ -9,3 +9,11 @@ s = (a + b + c) / 2
 # calculate the area  
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5  
 print('The area of the triangle is %0.2f' %area)   
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial
