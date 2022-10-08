3268d4452dd097d29aac9100cc36c09363a81599
it-mp4 <115347746+it-mp4@users.noreply.github.com>
2022-10-08 15:37:34 +0530

    Create # Code to show how we use docstrings in Python      def add(x, y):       """This function adds the values of x and y"""       return x + y       # Displaying the docstring of the add function   print( add.__doc__ )  .py

---
+++ # Code to show how we use docstrings in Python      def add(x, y):       """This function adds the values of x and y"""       return x + y       # Displaying the docstring of the add function   print( add.__doc__ )  .py
@@ -0,0 +1,15 @@
+# Code to show how we use # Code to show how we use docstrings in Python  
+  
+def add(x, y):  
+    """This function adds the values of x and y"""  
+    return x + y  
+   
+# Displaying the docstring of the add function  
+print( add.__doc__ )   
+  
+def add(x, y):  
+    """This function adds the values of x and y"""  
+    return x + y  
+   
+# Displaying the docstring of the add function  
+print( add.__doc__ )  
