ff31bf1a93b6e0423f6f087c2e02b6b8c85282e1
Rajat Matella <58916032+RajatMatella@users.noreply.github.com>
2022-10-15 15:32:50 +0530

    Create JavaT.py

---
+++ JavaT.py
@@ -0,0 +1,9 @@
+print("Welcome to javaTpoint.")  
+  
+a = 10  
+# Two objects are passed in print() function  
+print("a =", a)  
+  
+b = a  
+# Three objects are passed in print function  
+print('a =', a, '= b')  
